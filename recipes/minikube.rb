7fa3207ff2d3c29f67f8cfe0487ba1ffa6a934d3
Bob van den Heuvel <bvandenheuvel@schubergphilis.com>
2016-10-27 10:17:20 +0200

    (Pre)Define docker-machines network in LibVirt

--- recipes/minikube.rb
+++ recipes/minikube.rb
@@ -1,3 +1,6 @@
+# Use Chef cache as tmp location
+tmp_loc = Chef::Config[:file_cache_path]
+
 remote_file '/usr/local/bin/kubectl' do
   source node['bubble']['kubectl_download_url']
   mode '0755'
@@ -19,4 +22,25 @@ end
 bash 'kubectl_tabcompletion' do
   code 'echo "source <(kubectl completion bash)" >> /etc/bashrc'
   not_if { `cat /etc/bashrc`.include? 'source <(kubectl completion bash)' }
+end
+
+# Import libvirt configurations
+cookbook_file "#{tmp_loc}/net_docker-machines.xml" do
+  source 'libvirt/net_docker-machines.xml'
+  owner 'root'
+  group 'root'
+  mode 00644
+  not_if { ::File.exist?('/etc/libvirt/qemu/networks/docker-machines.xml') }
+end
+
+bash 'Configure_docker-machines_network' do
+  user 'root'
+  cwd "#{tmp_loc}"
+  code <<-EOH
+  virsh net-define net_docker-machines.xml
+  virsh net-start docker-machines
+  virsh net-autostart docker-machines
+  EOH
+  not_if { ::File.exist?('/etc/libvirt/qemu/networks/docker-machines.xml') }
+  notifies :request_reboot, 'reboot[Reboot for networking]', :delayed
 end
