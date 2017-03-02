507c3048fc6a7557101b0351556660cc2df962c4
Bob van den Heuvel <bvandenheuvel@schubergphilis.com>
2017-03-02 20:48:28 +0100

    Provide lists of packages directly to the packages resource
    to form a single yum transaction instead of separate calls

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,30 +1,24 @@
 # Packages for virt-manager and kvm
-%w(epel-release centos-release-qemu-ev vlgothic-fonts adwaita-gtk3-theme kvm
-   virt-manager libvirt virt-install qemu-kvm xauth dejavu-lgc-sans-fonts).each do |pkg|
-  package pkg do
-    action :install
-  end
+package %w(epel-release centos-release-qemu-ev vlgothic-fonts adwaita-gtk3-theme
+           kvm virt-manager libvirt virt-install qemu-kvm xauth
+           dejavu-lgc-sans-fonts) do
+  action :install
 end
 
 # Extra tools
-%w(git virt-top screen vim openssh-askpass supervisor deltarpm sshpass byobu bash-completion nmap).each do |pkg|
-  package pkg do
-    action :install
-  end
+package %w(git virt-top screen vim openssh-askpass supervisor deltarpm sshpass
+           byobu bash-completion nmap) do
+  action :install
 end
 
 # libguestfs-tools for manipulating images
-%w(libguestfs-tools).each do |pkg|
-  package pkg do
-    action :install
-  end
+package %w(libguestfs-tools) do
+  action :install
 end
 
 # Python packages for the deploy scripts
-%w(python-pip python-bottle python-argparse python-jinja2).each do |pkg|
-  package pkg do
-    action :install
-  end
+package %w(python-pip python-bottle python-argparse python-jinja2) do
+  action :install
 end
 
 # Packages for running marvin (compile scripts) on the bubble itself
@@ -34,11 +28,9 @@ package 'maven' do
 end
 include_recipe 'maven::default'
 
-%w(python-paramiko ws-commons-util genisoimage gcc python MySQL-python mariadb mysql-connector-python
-   ).each do |pkg|
-  package pkg do
-    action :install
-  end
+package %w(python-paramiko ws-commons-util genisoimage gcc python MySQL-python
+           mariadb mysql-connector-python) do
+  action :install
 end
 
 # required java & tools
@@ -47,23 +39,17 @@ package 'java-1.8.0-openjdk-devel.x86_64' do
   action :upgrade
 end
 
-%w(apache-commons-daemon-jsvc libffi-devel).each do |pkg|
-  package pkg do
-    action :install
-  end
+package %w(apache-commons-daemon-jsvc libffi-devel) do
+  action :install
 end
 
 # tools & clients
-%w(mariadb-server nc nfs-utils openssh-clients openssl-devel rpm-build setroubleshoot wget).each do |pkg|
-  package pkg do
-    action :install
-  end
+package %w(mariadb-server nc nfs-utils openssh-clients openssl-devel rpm-build
+           setroubleshoot wget) do
+  action :install
 end
 
 # python & ruby tooling
-%w(python-devel python-ecdsa python-setuptools rubygems).each do |pkg|
-  package pkg do
-    action :install
-  end
+package %w(python-devel python-ecdsa python-setuptools rubygems) do
+  action :install
 end
-
